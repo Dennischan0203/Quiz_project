f8407337b2d79b8f2fa4287ca150a89ad0c74e06
Dennis <dennis20020323@gmail.com>
2025-11-21 04:56:29 +1030

    made a static website

---
+++ Quiz/script.js
@@ -0,0 +1,6 @@
+const startQuiz = document.getElementById('startScreen');
+const quizing = document.getElementById('quizing');
+const quizResult = document.getElementById('resultScreen');
+const quizContainer = document.getElementById('quiz-container');
+
+quizContainer.innerHTML = startQuiz.innerHTML;
