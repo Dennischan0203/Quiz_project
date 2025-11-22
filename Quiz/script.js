7b2439a9c7d1287955543303b543693021a7990a
Dennis <dennis20020323@gmail.com>
2025-11-22 20:15:31 +1030

    make the website runnable, all the button are functionable

--- Quiz/script.js
+++ Quiz/script.js
@@ -7,48 +7,99 @@ quizContainer.innerHTML = startQuiz.innerHTML;
 
 const startButton = document.getElementById('start');
 
-startButton.addEventListener('click', gotoQuestions);
-
-let questionNO = 0;
-let progress_bar = 0;
-let totalScore = 0;
-const totalQuestions = 5;
-
 class Quiz{
     constructor(title, answers){
         this.title = title;
         let r_num;
         this.answers = [];
         let length = answers.length;
-        for(let i = length+1; i > 1; i--){
-            r_num = Math.floor(Math.random()*length);
+        for(let i = length; i > 0; i--){
+            r_num = Math.floor(Math.random()*i);
             this.answers.push(answers[r_num]);
             answers.splice(r_num,1);
-            length--;
         };
     }
 }
 
-function gotoQuestions(){
+let questionNO = 0;
+let progress_bar = 0;
+let totalScore = 0;
+const totalQuestions = 5;
+
+const q1 = new Quiz("What is 1",['1','2','3','4']);
+const q2 = new Quiz("What is 2",['1','2','3','4']);
+const q3 = new Quiz("What is 3",['1','2','3','4']);
+const q4 = new Quiz("What is 4",['1','2','3','4']);
+const q5 = new Quiz("What is 5",['1','2','3','4']);
+
+// Start the quiz
+
+startButton.addEventListener('click', async()=>{
+    gotoQuestions(q1,1);
+    await nextQuestion(q2,2);
+    await nextQuestion(q3,3);
+    await nextQuestion(q4,4);
+    await nextQuestion(q5,5);
+    gotoResult();
+}, { once: true });
+
+
+
+function gotoQuestions(q, q_no){
     quizContainer.innerHTML = quizing.innerHTML;
+    const current_progress = ((q_no-1)/totalQuestions)*100;
     const question = document.getElementById('question');
     const question_no = document.getElementById('current-question');
     const total_question = document.getElementById('total-question');
     const score = document.getElementById('current-score');
     const bar = document.querySelector('div.progress');
-    const q1 = new Quiz("What is XXXX",['1','2','3','4']);
-    question.textContent = q1.title;
+    question.textContent = q.title;
     const fragment = document.createDocumentFragment();
-    for (let i = 0; i < q1.answers.length; i++) {
+    for (let i = 0; i < q.answers.length; i++) {
         const a = document.createElement('button');
-        a.textContent = q1.answers[i];
+        a.textContent = q.answers[i];
         a.classList.add('answers');
         fragment.appendChild(a);
     }
     document.querySelector('div.select-section').appendChild(fragment);
-    question_no.textContent = questionNO;
+    question_no.textContent = q_no;
     questionNO++;
     total_question.textContent = totalQuestions;
     score.textContent = totalScore;
-    bar.className = 'progress0';
+    bar.id = `progress${current_progress}`;
+}
+
+async function nextQuestion(q,q_no){
+    const answer_buttons = document.querySelector('.select-section');
+    await new Promise(resolve => {
+        answer_buttons.addEventListener('click', (e)=>{
+            if(e.target.classList.contains('answers')){
+                gotoQuestions(q,q_no);
+            };
+            resolve();
+        }, { once: true });
+    });
+}
+
+async function gotoResult(){
+    const answer_buttons = document.querySelector('.select-section');
+    await new Promise(resolve => {
+        answer_buttons.addEventListener('click', (e)=>{
+            if(e.target.classList.contains('answers')){
+                quizContainer.innerHTML = quizResult.innerHTML;
+            };
+            resolve();
+        }, { once: true });
+    });
+    console.log('hi')
+    const restart = document.querySelector('#restart');
+    console.log('hi2')
+    restart.addEventListener('click',async()=>{
+        gotoQuestions(q1,1);
+        await nextQuestion(q2,2);
+        await nextQuestion(q3,3);
+        await nextQuestion(q4,4);
+        await nextQuestion(q5,5);
+        gotoResult();
+    })
 }
